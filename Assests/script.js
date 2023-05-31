b3853938a2546c2f0368179b180c8447d63b47b7
Jayne Valverde <jaynevalverde@gmail.com>
2023-05-30 21:41:34 -0500

    java edits

--- Assests/script.js
+++ Assests/script.js
@@ -10,95 +10,49 @@ var sampleData = [{
 }];
 
 //function to find all data for both APIs
-function searchAPI() {    
-    //  var tmdbUrl = "https://api.themoviedb.org/3/movie/343611?api_key=0d6d6b4bebecbfdfd42593dcd6f307e6"
-    //  fetch(tmdbUrl, {
-    //      method: 'GET',
-    //      header: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDZkNmI0YmViZWNiZmRmZDQyNTkzZGNkNmYzMDdlNiIsInN1YiI6IjY0NmMxZmVkMmJjZjY3MDBlM2JjMWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEtgFi5IKAEdaFJ8zNyOegQkkKebjAHKwb61zNPj5_I', 'accept': 'application/json' }
-    //  }).then(function (response) {
-    //      console.log(response);
-    //      return response.json();
-    //  }).then(function (data) {
-    //      console.log("tmdbapi:", data)
-    //      var tmdbApiId = data.imdb_id;
-    //      var imageURL = URL.createObjectURL(data);
-    //      var watchmodeApiUrl = `https://api.watchmode.com/v1/title/${tmdbApiId}/details/?apiKey=Pmd5eUDJou34DMGyeaDChDeFLhOJHRxVt1MfzboM&append_to_response=sources`
+// function searchAPI() {
+//     var tmdbUrl = "https://api.themoviedb.org/3/movie/343611?api_key=0d6d6b4bebecbfdfd42593dcd6f307e6"
+//     fetch(tmdbUrl, {
+//         method: 'GET',
+//         header: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDZkNmI0YmViZWNiZmRmZDQyNTkzZGNkNmYzMDdlNiIsInN1YiI6IjY0NmMxZmVkMmJjZjY3MDBlM2JjMWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEtgFi5IKAEdaFJ8zNyOegQkkKebjAHKwb61zNPj5_I', 'accept': 'application/json' }
+//     }).then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     }).then(function (data) {
+//         console.log("tmdbapi:", data)
+//         var tmdbApiId = data.imdb_id;
 
-    //      fetch(watchmodeApiUrl).then(function (response) {
-    //          return response.json();
-    //      }).then(function (data) {
-    //          console.log("watchmodeapi:", data);
-    //      })
-    //  });
-     return sampleData;
-    
-};
+//         var watchmodeApiUrl = `https://api.watchmode.com/v1/title/${tmdbApiId}/details/?apiKey=hr9u8TGmWHKemrBe5k1uVAYuLpindRYF8el3C1jp&append_to_response=sources`
 
-//searchAPI();
-
-var searchButton = document.getElementById("searchButton")
-var tmdbApiKey = "0d6d6b4bebecbfdfd42593dcd6f307e6"
-var watchmodeApiKey = "Pmd5eUDJou34DMGyeaDChDeFLhOJHRxVt1MfzboM"
-
-//function to find all data for both APIs
-function searchAPI(movie) {
-    console.log("move string: ",movie)
-   var tmdbUrl = "https://api.themoviedb.org/3/movie/343611?api_key=0d6d6b4bebecbfdfd42593dcd6f307e6"
-   //var tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=${movie}` 
-   //var tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=Jack+Reacher`
-   fetch(tmdbUrl, {
-        method: 'GET',
-        header: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDZkNmI0YmViZWNiZmRmZDQyNTkzZGNkNmYzMDdlNiIsInN1YiI6IjY0NmMxZmVkMmJjZjY3MDBlM2JjMWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEtgFi5IKAEdaFJ8zNyOegQkkKebjAHKwb61zNPj5_I', 'accept': 'application/json' }
-    }).then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data) {
-        console.log("tmdbapi:", data)
-        var tmdbApiId = data.imdb_id;
-  
-        var watchmodeApiUrl = `https://api.watchmode.com/v1/title/${tmdbApiId}/details/?apiKey=${watchmodeApiKey}}&append_to_response=sources`
-
-        fetch(watchmodeApiUrl).then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log("watchmodeapi:", data);
-        })
-    });
-};
+//         fetch(watchmodeApiUrl).then(function (response) {
+//             return response.json();
+//         }).then(function (data) {
+//             console.log("watchmodeapi:", data);
+//         })
+//     });
+// };
 
 // searchAPI();
 
 //function to display search results
+// function displayResults(data) {
+// for (var i = 0; i < data.length; i++) {
+//     var movieNameEl = document.querySelector('#movieName');
+//     var posterEl = document.querySelector('#poster-image');
+//     var whereToWatchEl = document.querySelector('#whereToWatch');
+//     var dateEl = document.querySelector('#release-date');
 
-// function displayResults(){
+//     movieNameEl.textContent = data[i].title;
+//     posterEl.textContent = data[i].poster;
+//     whereToWatchEl.textContent = data[i].sources.web_url;
+//     dateEl.textContent = data[i].release_date;
 
+//     console.log('title', movieNameEl);
+//     console.log('poster', posterEl);
+//     console.log('where to watch', whereToWatchEl);
+//     console.log('date', dateEl);
+// }
 // };
-
-function displayResults(data) {
-    console.log(data);
-    console.log(data.length);
-for (var i = 0; i < data.length; i++) {
-    var movieNameEl = document.querySelector('#movieName');
-    var posterEl = document.querySelector('#poster-image');
-    //scr attribute for image
-    var whereToWatchEl = document.querySelector('#whereToWatch');
-    var dateEl = document.querySelector('#release-date');
-
-console.log(sampleData);
-
-    movieNameEl.textContent = data[i].title;
-    posterEl.textContent = data[i].poster_path;
-    whereToWatchEl.textContent = data[i].sources.web_url;
-    dateEl.textContent = data[i].release_date;
-
-    //posterEl.src = imageURL;
-
-    console.log('title', movieNameEl);
-    console.log('poster', posterEl);
-    console.log('where to watch', whereToWatchEl);
-    console.log('date', dateEl);
-}
-};
 
 
 //movie that user chooses 
