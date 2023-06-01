e74d1d0a5d0531c458c0eb73688f7afd4c2f6001
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-06-01 16:28:36 -0500

    update

--- Assests/script.js
+++ Assests/script.js
@@ -1,18 +1,6 @@
-
-
-
-
 //variables
 var searchButton = document.querySelector("#searchButton")
 
-var sampleData = [{
-    release_date: "2016-10-19",
-    title: "Jack Reacher: Never Go Back",
-    poster_path: "/cOg3UT2NYWHZxp41vpxAnVCOC4M.jpg",
-    sources: {
-        web_url: "https://www.amazon.com/gp/video/detail/amzn1.dv.gti.acabc6b0-07af-2d3f-d166-ccd78097dd17?tag="
-    }
-}];
 var searchButton = document.getElementById("searchButton")
 var tmdbApiKey = "0d6d6b4bebecbfdfd42593dcd6f307e6"
 var watchmodeApiKey = "Pmd5eUDJou34DMGyeaDChDeFLhOJHRxVt1MfzboM"
@@ -21,19 +9,33 @@ var watchmodeApiKey = "Pmd5eUDJou34DMGyeaDChDeFLhOJHRxVt1MfzboM"
 var searchHistory = [];
 
 //function to find all data for both APIs
-// function searchAPI() {
-//     var tmdbUrl = "https://api.themoviedb.org/3/movie/343611?api_key=0d6d6b4bebecbfdfd42593dcd6f307e6"
-//     fetch(tmdbUrl, {
-//         method: 'GET',
-//         header: { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDZkNmI0YmViZWNiZmRmZDQyNTkzZGNkNmYzMDdlNiIsInN1YiI6IjY0NmMxZmVkMmJjZjY3MDBlM2JjMWE2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEtgFi5IKAEdaFJ8zNyOegQkkKebjAHKwb61zNPj5_I', 'accept': 'application/json' }
-//     }).then(function (response) {
-//         console.log(response);
-//         return response.json();
-//     }).then(function (data) {
-//         console.log("tmdbapi:", data)
-//         var tmdbApiId = data.imdb_id;
+function searchAPI(movie) {
+    console.log("move string: ", movie)
+    //tmdb api call below  provides ID for watchmodeapi 
+    var tmdbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbApiKey}&query=${movie}`
+    fetch(tmdbUrl).then(function (response) {
+        console.log(response);
+        return response.json();
+    }).then(function (data) {
+        console.log("tmdbapi:", data)
+        var tmdbApiId = data.results[0].id;
+        var tmdbTitle = data.results[0].title;
+        console.log("tmdbapiId ", tmdbApiId);
+        console.log("tmdbTitle: ", tmdbTitle);
+        var tmdbMovieId = "movie-" + tmdbApiId;
+        console.log(tmdbMovieId)
+        //using id from tmdbapi , we're calling the watchmode api below to provide display data
+        var watchmodeApiUrl = `https://api.watchmode.com/v1/title/${tmdbMovieId}/details/?apiKey=${watchmodeApiKey}&append_to_response=sources`
+        console.log(watchmodeApiUrl);
+        fetch(watchmodeApiUrl).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log("watchmodeapi:", data);
+            displayResults(data);
+        })
+    });
+};
 
-// searchAPI();
 
 function displayResults(data) {
 
@@ -44,14 +46,14 @@ function displayResults(data) {
     var dateEl = document.querySelector('#release-date');
     //updating index with data from watchmode api
     movieNameEl.textContent = data.title;
-
+    
     dateEl.textContent = data.release_date;
     posterEl.src = data.poster;
 
-    for (i = 0; i < data.sources.length; i++) {
-        var format = data.sources[i].format;
+    for (i = 0; i<data.sources.length; i++ ) {
+        var format=data.sources[i].format;
         if (format === "HD") {
-            console.log(data.sources[i].name);
+            console.log (data.sources[i].name);
             var stream = document.createElement("a");
             stream.innerHTML = `<a href="${data.sources[i].web_url}">${data.sources[i].name}</a>`
             whereToWatchEl.append(stream);
@@ -66,16 +68,17 @@ function displayResults(data) {
 };
 
 
-function displayResults(data) {
-
-    function userMovieChoice(movie) {
-        console.log("movie: ", movie);
-        var movieEntered = document.getElementById("movieInput").value;
-        searchAPI(movieEntered);
-    };
-
+//grab value/movie entered by user - DONE
+function userMovieChoice(movie){
+    console.log("movie: ", movie);
+    var movieEntered = document.getElementById("movieInput").value;
+    searchAPI(movieEntered);
+};
 
 
+//event listener and search button function - DONE
+ function searchBtn(event) {
+     event.preventDefault();
 
     var movieSearched = document.getElementById("movieInput").value;
 
@@ -90,40 +93,9 @@ function displayResults(data) {
         searchHistory.push(movieSearched);
         savedata();
     }
+};
+searchButton.addEventListener("click",searchBtn);
 
-
-    //search button event listener and function - DONE
-
-    function searchBtn(event) {
-        event.preventDefault();
-        var movieSearched = document.getElementById("movieInput").value;
-        if (!movieSearched) {
-            console.error("You need a search input value!");
-            return;
-        }
-        displayResults(searchAPI());
-        //userMovieChoice(movieSearched);
-    };
-    searchButton.addEventListener("click", searchBtn);
-
-
-
-    // function searchBtn(event) {
-    //     event.preventDefault();
-
-    //event listener and search button function - DONE
-    function searchBtn(event) {
-        event.preventDefault();
-
-        var movieSearched = document.getElementById("movieInput").value;
-
-        if (!movieSearched) {
-            console.error('You need a search input value!');
-            return;
-        }
-        userMovieChoice(movieSearched);
-    };
-    searchButton.addEventListener("click", searchBtn);
 
 
 //function to save movie to local storage- emily 5/31
@@ -134,6 +106,3 @@ function savedata() {
 };
   
   
-
-}
-
