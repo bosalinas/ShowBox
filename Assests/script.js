3528b420b3734561f864191a70a0f0d643b438e6
Jayne Valverde <jaynevalverde@gmail.com>
2023-06-02 17:01:36 -0500

    scroll, dark, list

--- Assests/script.js
+++ Assests/script.js
@@ -8,6 +8,41 @@ var watchmodeApiKey = "Pmd5eUDJou34DMGyeaDChDeFLhOJHRxVt1MfzboM"
 //emily
 var searchHistory = [];
 
+// TOGGLE SWITCH 
+let toggleSwitch = document.querySelector("input");
+
+toggleSwitch.addEventListener("change",(e) => {
+    let body = document.querySelector("body");
+    let title = document.querySelector("#grid-container");
+    let font = document.querySelector("h2");
+    let fontTwo = document.querySelector("#release-date");
+    let fontThree = document.querySelector("#movieName");
+    let fontFour = document.querySelector("#whereWatch");
+    let button = document.querySelector("button");
+    let buttonS = document.querySelector("#buttonS");
+    
+
+    if (e.target.checked) {
+        body.style.backgroundColor = "#242124";
+        title.style.backgroundColor = "#eb3e3e";
+        font.style.color = "#f3e6d8";
+        fontTwo.style.color = "#f3e6d8";
+        fontThree.style.color = "#f3e6d8";
+        fontFour.style.color = "#f3e6d8";
+        button.style.color = "#f3e6d8";
+        buttonS.style.color = "#f3e6d8";
+    } else {
+        body.style.backgroundColor = "#f3e6d8";
+        title.style.backgroundColor = "#eb3e3e";
+        font.style.color = "#000000";
+        fontTwo.style.color = "#000000";
+        fontThree.style.color = "#000000";
+        fontFour.style.color = "#000000";
+        button.style.color = "#000000";
+        buttonS.style.color = "#000000"
+    }
+});
+
 //function to find all data for both APIs
 function searchAPI(movie) {
     console.log("move string: ", movie)
@@ -44,20 +79,22 @@ function displayResults(data) {
     var posterEl = document.querySelector('#poster-image');
     var whereToWatchEl = document.querySelector('#whereToWatch');
     var dateEl = document.querySelector('#release-date');
+    var scrollBoxEl = document.querySelector('#scrollBox');
+
     //updating index with data from watchmode api
     movieNameEl.textContent = data.title;
-    
+
     dateEl.textContent = data.release_date;
     posterEl.src = data.poster;
 
-    for (i = 0; i<data.sources.length; i++ ) {
-        var format=data.sources[i].format;
+    for (i = 0; i < data.sources.length; i++) {
+        var format = data.sources[i].format;
         if (format === "HD") {
-            console.log (data.sources[i].name);
-            var stream = document.createElement("a");
+            console.log(data.sources[i].name);
+            var stream = document.createElement("li");
             stream.innerHTML = `<a href="${data.sources[i].web_url}">${data.sources[i].name}</a>`
             whereToWatchEl.append(stream);
-
+            
         }
     }
 
@@ -69,7 +106,7 @@ function displayResults(data) {
 
 
 //grab value/movie entered by user - DONE
-function userMovieChoice(movie){
+function userMovieChoice(movie) {
     console.log("movie: ", movie);
     var movieEntered = document.getElementById("movieInput").value;
     searchAPI(movieEntered);
@@ -77,8 +114,8 @@ function userMovieChoice(movie){
 
 
 //event listener and search button function - DONE
- function searchBtn(event) {
-     event.preventDefault();
+function searchBtn(event) {
+    event.preventDefault();
 
     var movieSearched = document.getElementById("movieInput").value;
 
@@ -94,13 +131,36 @@ function userMovieChoice(movie){
         savedata();
     }
 };
-searchButton.addEventListener("click",searchBtn);
+searchButton.addEventListener("click", searchBtn);
 
 
 
-//function to save movie to local storage- emily 5/31
-
+//function to save movie to local storage- emily 5/31- done
 function savedata() {
-    localStorage.setItem(movieInput, JSON.stringify(searchHistory));
-    console.log(savedata);
+    localStorage.setItem("movieInput", JSON.stringify(searchHistory));
+    console.log();
 };
+//button event listener for saved searches
+document.getElementById("savedButton").addEventListener("click", displaysavedata);
+var savedSearches = document.getElementById("savedSearches");
+function displaysavedata() {
+    var historyList = document.querySelector(".movieInput");
+    historyList.innerHTML = "";
+    searchHistory.forEach(function (movieSearched) {
+        var li = document.createElement("li");
+        li.textContent = posterEl;
+        li.addEventListener("click", function () {
+            cityInput.value = city;
+            getCurrentWeather(city);
+        });
+        historyList.appendChild(li);
+    });
+}
+// Load search history from local storage
+function loadSearchHistory() {
+    var history = localStorage.getItem("history");
+    if (history) {
+        searchHistory = JSON.parse(history);
+        displaysavedata();
+    }
+}
